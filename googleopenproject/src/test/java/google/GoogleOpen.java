118bc27cf52e37a76f1e1e41b1ca4ae71adbf440
V <V@DESKTOP-T0GPNL0>
2024-05-25 18:58:52 +0530

    Initial Commit

---
+++ googleopenproject/src/test/java/google/GoogleOpen.java
@@ -0,0 +1,24 @@
+package google;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleOpen {
+	@Test
+	public void googleOpenChrome() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		driver.get("https://www.google.com/");
+		
+	}
+
+}
